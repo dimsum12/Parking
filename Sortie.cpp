71c77d96635a7f422d2e42dfd89f6c421cbfead4
qdupont <df>
2014-03-13 18:01:48 +0100

    kill les voituriers au signal SIGUSR2 = ça marche

--- Sortie.cpp
+++ Sortie.cpp
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <vector>
 //-------------------------------------------------------- Include personnel
 
 #include "Sortie.h"
@@ -23,27 +24,39 @@
 
 static int s_voiture;
 static void handler_destruction(int noSignal);
+static vector<pid_t> vVoituresASortir; //Contient tous les PID des voituriers en cours de sortie
 
 void Sortie(){
 
-    //Creation du Handler de Destruction_Sortie
+	//Creation du Handler de Signal SIGUSR2 de Destruction_Sortie
     struct sigaction action_siguser2;
     action_siguser2.sa_handler = handler_destruction;
     action_siguser2.sa_flags = 0;
     sigaction(SIGUSR2, &action_siguser2, NULL);
+
 
 	unsigned int num_place = 0;
     s_voiture = open(pathPipeSortie, O_RDONLY);
 
     for(;;){
     	read(s_voiture,&num_place,sizeof(num_place));
-    	SortirVoiture(num_place);
+    	vVoituresASortir.push_back(SortirVoiture(num_place));
     }
 
 }
 
 
 static void handler_destruction(int noSignal) {
+
+	// Destruction de toutes les tâches SortirVoiture en cours d'éxecution
+	vector<pid_t>::iterator it ;
+	for(it=vVoituresASortir.begin();it!=vVoituresASortir.end();it++)
+	{
+		if ( *(it) != -	1 )
+		{
+			kill(*(it),SIGUSR2);
+		}
+	}
 	exit(0);
 }
 
